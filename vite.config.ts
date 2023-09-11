8b5c2ff7e5d6365957b7349b36aa9108998a8664
huangshaofeng <huangshaofeng@xiaoyezi.com>
2023-09-11 10:39:28 +0800

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "nest-demo-web",
   server: {
     proxy: {
       // 字符串简写写法：http://localhost:5173/foo -> http://localhost:4567/foo
